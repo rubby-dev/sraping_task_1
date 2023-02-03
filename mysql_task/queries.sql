7ada8b3dfb85aa1342037fdd9f4e977a6d0f854f
Umar <umarfarooq4973@gmail.com>
2023-02-04 00:20:38 +0500

    Improved and optimized queries

--- mysql_task/queries.sql
+++ mysql_task/queries.sql
@@ -2,29 +2,37 @@
 
 SELECT product_name, product_img_url, product_url, product_price_min, product_short_description
 FROM grommet_products
+JOIN grommet_product_categories ON grommet_products.id = grommet_product_categories.product_id
+JOIN grommet_gifts_categories ON grommet_product_categories.product_category_id = grommet_gifts_categories.id
 WHERE is_sold_out = 0
-  AND id IN (SELECT product_id FROM grommet_product_categories WHERE product_category_id IN (SELECT id FROM grommet_gifts_categories WHERE sub_category = "Jewelry"));
+AND grommet_gifts_categories.sub_category = "Jewelry";
 
 
 # 2nd query
 
 SELECT product_name, product_img_url, product_url, product_price_min, product_short_description
 FROM grommet_products
+JOIN grommet_product_to_keyword ON grommet_products.id = grommet_product_to_keyword.product_id
+JOIN grommet_product_keywords ON grommet_product_to_keyword.keyword_id = grommet_product_keywords.id
 WHERE is_sold_out = 0
-  AND id IN (SELECT product_id FROM grommet_product_to_keyword WHERE keyword_id IN (SELECT id FROM grommet_product_keywords WHERE keyword = "Hair accessor"));
+AND grommet_product_keywords.keyword = "Hair accessor";
 
 
 # 3rd query
 
-SELECT
-    p1.product_name,
-    p2.product_img_url,
-    p2.product_url,
-    p2.product_price_min,
-    p2.product_short_description
-FROM
-    grommet_products p1
-        JOIN grommet_product_categories pc ON p1.id = pc.product_id
-        JOIN grommet_product_to_keyword ptk ON p1.id = ptk.product_id
-        JOIN grommet_products p2 ON p2.id = p1.id
-        JOIN grommet_gifts_categories g ON g.id = p1.id;
+SELECT 
+  p.product_name, 
+  p.product_img_url, 
+  p.product_url, 
+  p.product_price_min, 
+  p.product_short_description
+FROM 
+  grommet_products p 
+  LEFT JOIN grommet_product_categories pc ON p.id = pc.product_id 
+  LEFT JOIN grommet_gifts_categories gc ON pc.product_category_id = gc.id 
+  LEFT JOIN grommet_product_to_keyword pk ON p.id = pk.product_id 
+  LEFT JOIN grommet_product_keywords k ON pk.keyword_id = k.id 
+WHERE 
+  (gc.sub_category = 'Beauty & Personal Care' OR gc.sub_category = 'Skincare' 
+  OR k.keyword = 'Aromatherapy') 
+  AND p.is_sold_out != 'yes';
